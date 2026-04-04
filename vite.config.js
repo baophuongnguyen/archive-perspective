6cab2047047ecab8166dbb95bd75c9bb49ced896
Nguyen Bao Phuong <baophuong2007@gmail.com>
2026-04-05 01:16:58 +0700

    Update config and content for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/archive-perspective/", // <--- Must have the slashes on BOTH sides!
+  base: "/archive-perspective/", // <--- CHECK THIS: Slashes on both sides!
 })
